aa5806b265323308cfdcf3b55dd73f21142e69f6
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-16 02:15:00 -0300

    :lipstick: feat: transactions table component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from './styles';
 import { Summary } from '../Summary';
+import { TransactionsTable } from '../TransactionsTable';
 
 export const Dashboard: React.FC = () => {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 };
