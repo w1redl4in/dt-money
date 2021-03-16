3d7702327a97a3fa7e7865b8e0c1687a3720b05e
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-16 02:04:22 -0300

    :memo: feat: dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+import { Summary } from '../Summary';
+
+export const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  );
+};
