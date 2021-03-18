ccca64e7ebcadd175e96026500027fdbab70a6c6
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-17 21:56:50 -0300

    :lipstick: refacotr: abstracting modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyles } from './styles/global';
+import Modal from 'react-modal';
+import { useCallback, useState } from 'react';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
+
+  const handleOpenNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen((prevState) => !prevState);
+  }, []);
+
+  const handleCloseNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen((prevState) => !prevState);
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyles />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import Logo from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export const Header: React.FC = () => {
+type HeaderProps = {
+  onOpenNewTransactionModal: () => void;
+};
+
+export const Header: React.FC<HeaderProps> = ({
+  onOpenNewTransactionModal,
+}) => {
   return (
     <Container>
       <Content>
         <img src={Logo} alt="dt money" />
-        <button type="submit">Nova transação</button>
+        <button type="submit" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
