c7297e232abdb906312f1b58302461f90f41a866
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-19 01:20:58 -0300

    :chart_with_up_wards_trend: feat: calculating summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,8 +2,32 @@ import { Container } from './styles';
 import IncomeImage from '../../assets/income.svg';
 import OutcomeImage from '../../assets/outcome.svg';
 import TotalImage from '../../assets/total.svg';
+import { useTransactionsHook } from '../../TransactionsContext';
+import { formatCurrency } from '../../util/format';
+import Skeleton from 'react-loading-skeleton';
 
 export const Summary: React.FC = () => {
+  const { transactions, isLoading } = useTransactionsHook();
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'deposit') {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
+
   return (
     <Container>
       <div>
@@ -11,21 +35,27 @@ export const Summary: React.FC = () => {
           <p>Entradas</p>
           <img src={IncomeImage} alt="income dt money" />
         </header>
-        <strong>R$ 1000,00</strong>
+        <strong>
+          {isLoading ? <Skeleton /> : formatCurrency(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={OutcomeImage} alt="outcome dt money" />
         </header>
-        <strong>- R$ 1000,00</strong>
+        <strong>
+          {isLoading ? <Skeleton /> : formatCurrency(summary.withdraws)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Total</p>
           <img src={TotalImage} alt="income dt money" />
         </header>
-        <strong>R$ 500,00</strong>
+        <strong>
+          {isLoading ? <Skeleton /> : formatCurrency(summary.total)}
+        </strong>
       </div>
     </Container>
   );
